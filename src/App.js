358c9c35245c1093a317c20b0f8f8ddd4ee7be25
sophiepavlova <olni.pavlova@gmail.com>
2026-07-02 10:32:35 -0700

    Refactor: add components folder and move components there

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { useEffect, useReducer } from 'react';
 // import DateCounter from './DateCounter.js';
-import Main from './Main.js';
-import Header from './Header.js';
-import Loader from './Loader.js';
-import Error from './Error.js';
-import StartScreen from './StartScreen.js';
+import Main from './components/Main.js';
+import Header from './components/Header.js';
+import Loader from './components/Loader.js';
+import Error from './components/Error.js';
+import StartScreen from './components/StartScreen.js';
+import Question from './components/Question.js';
 
 const initialState = {
   questions: [],
   //"loading", 'error', 'ready', 'active', 'finished'
   status: 'loading',
+  index: 0,
 };
+
 function reducer(state, action) {
   switch (action.type) {
     case 'dataReceived':
       return { ...state, questions: action.payload, status: 'ready' };
     case 'dataFailed':
       return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
     default:
       throw new Error('Action is unknown');
   }
@@ -24,7 +29,10 @@ function reducer(state, action) {
 
 export default function App() {
   // const [state, dispatch] = useReducer(reducer, initialState);
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState,
+  );
   const numQuestions = questions.length;
 
   useEffect(function () {
@@ -39,7 +47,10 @@ export default function App() {
       <Main>
         {status === 'loading' && <Loader />}
         {status === 'error' && <Error />}
-        {status === 'ready' && <StartScreen numQuestions={numQuestions} />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && <Question question={questions[index]} />}
       </Main>
     </div>
   );
