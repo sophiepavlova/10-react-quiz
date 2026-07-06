f63ae10a387a0a07e713e97dd16851965da30c72
sophiepavlova <olni.pavlova@gmail.com>
2026-07-06 13:05:23 -0700

    WIP, add the finish screen

---
+++ src/components/App.js
@@ -0,0 +1,117 @@
+import { useEffect, useReducer } from 'react';
+// import DateCounter from './DateCounter.js';
+import Main from './Main.js';
+import Header from './Header.js';
+import Loader from './Loader.js';
+import Error from './Error.js';
+import StartScreen from './StartScreen.js';
+import Question from './Question.js';
+import NextButton from './NextButton.js';
+import Progress from './Progress.js';
+import FinishScreen from './FinishScreen.js';
+
+const initialState = {
+  questions: [],
+  //"loading", 'error', 'ready', 'active', 'finished'
+  status: 'loading',
+  index: 0,
+  answer: null,
+  points: 0,
+  highscore: 0,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'dataReceived':
+      return { ...state, questions: action.payload, status: 'ready' };
+    case 'dataFailed':
+      return { ...state, status: 'error' };
+    case 'start':
+      return { ...state, status: 'active' };
+    case 'newAnswer':
+      const question = state.questions.at(state.index);
+      return {
+        ...state,
+        answer: action.payload,
+        points:
+          action.payload === question.correctOption
+            ? state.points + question.points
+            : state.points,
+      };
+    case 'nextQuestion':
+      return { ...state, index: state.index + 1, answer: null };
+    case 'finish':
+      return {
+        ...state,
+        status: 'finished',
+        highscore:
+          state.points > state.highscore ? state.points : state.highscore,
+      };
+    default:
+      throw new Error('Action is unknown');
+  }
+}
+
+export default function App() {
+  // const [state, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index, answer, points, highscore }, dispatch] =
+    useReducer(reducer, initialState);
+  const numQuestions = questions.length;
+  console.log(questions);
+  // const possiblePoints = questions.reduce(
+  //   (acc, question) => acc + question.points,
+  //   0,
+  // );
+  const maxPossiblePoints = questions.reduce(
+    (prev, cur) => prev + cur.points,
+    0,
+  );
+
+  useEffect(function () {
+    fetch('http://localhost:8000/questions')
+      .then((res) => res.json())
+      .then((data) => dispatch({ type: 'dataReceived', payload: data }))
+      .catch((err) => dispatch({ type: 'dataFailed' }));
+  }, []);
+  return (
+    <div className='app'>
+      <Header />
+      <Main>
+        {status === 'loading' && <Loader />}
+        {status === 'error' && <Error />}
+        {status === 'ready' && (
+          <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
+        )}
+        {status === 'active' && (
+          <>
+            <Progress
+              index={index}
+              numQuestions={numQuestions}
+              points={points}
+              maxPossiblePoints={maxPossiblePoints}
+              answer={answer}
+            />
+            <Question
+              question={questions[index]}
+              answer={answer}
+              dispatch={dispatch}
+            />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              index={index}
+              numQuestions={numQuestions}
+            />
+          </>
+        )}
+        {status === 'finished' && (
+          <FinishScreen
+            points={points}
+            maxPossiblePoints={maxPossiblePoints}
+            highscore={highscore}
+          />
+        )}
+      </Main>
+    </div>
+  );
+}
--- src/components/Question.js
+++ src/components/Question.js
@@ -1,11 +1,12 @@
 import Options from './Options';
 
-function Question({ question }) {
+function Question({ question, answer, dispatch }) {
   console.log(question);
+
   return (
     <div>
       <h4>{question.question}</h4>
-      <Options question={question} />
+      <Options question={question} answer={answer} dispatch={dispatch} />
     </div>
   );
 }
